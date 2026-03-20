ea5dabed0a66277c31af6132491c4d6964d37ca7
kishannnpatel <kishan.patel21@outlook.fr>
2026-03-20 14:23:57 +0100

    Ajout exception ChargerFichierException

--- src/main/java/universite_Paris8/iut/qdev/tp2026/gr14/services/interfaces/IQuestionnaireServices.java
+++ src/main/java/universite_Paris8/iut/qdev/tp2026/gr14/services/interfaces/IQuestionnaireServices.java
@@ -5,12 +5,13 @@ import java.util.List;
 import universite_Paris8.iut.qdev.tp2026.gr14.entities.dto.QuestionDTO;
 import universite_Paris8.iut.qdev.tp2026.gr14.entities.dto.QuestionnaireDTO;
 import universite_Paris8.iut.qdev.tp2026.gr14.utils.exceptions.CSVInexistantException;
+import universite_Paris8.iut.qdev.tp2026.gr14.utils.exceptions.ChargerFichierException;
 import universite_Paris8.iut.qdev.tp2026.gr14.utils.exceptions.DonneesCorrompuesException;
 
 public interface IQuestionnaireServices {
 
     List<QuestionnaireDTO> parcourirQuestionnaireDTO(String path)
-            throws CSVInexistantException, DonneesCorrompuesException;
+            throws ChargerFichierException;
 
     List<QuestionDTO> chargerFichier(String path)
             throws CSVInexistantException, DonneesCorrompuesException;
